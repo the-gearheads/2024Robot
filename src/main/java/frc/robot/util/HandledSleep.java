8db6bd00a381b1eb7694fffbde6fdfcd48681340
person4268 <28717044+person4268@users.noreply.github.com>
2024-02-15 13:53:12 -0500

    got tired of waiting so handledsleep does nothing in sim now

--- src/main/java/frc/robot/util/HandledSleep.java
+++ src/main/java/frc/robot/util/HandledSleep.java
@@ -1,7 +1,10 @@
 package frc.robot.util;
 
+import frc.robot.Robot;
+
 public class HandledSleep {
     public static void sleep(long delayMs) {
+        if(Robot.isSimulation()) return;
         try {
             Thread.sleep(delayMs);
         } catch(InterruptedException e) {
