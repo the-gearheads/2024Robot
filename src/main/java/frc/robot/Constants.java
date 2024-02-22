5be6e696e0af2019a5d4bce1a0347d7ec599d624
person4268 <28717044+person4268@users.noreply.github.com>
2024-02-22 17:29:32 -0500

    drive motor on-mc pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,7 +72,7 @@ public final class Constants {
 
 
     public static final double[] STEER_PIDF = {1, 0, 0, 0}; // apparently just a P value of 1 worked for us??? i wanna test that a bit more throughly
-    public static final double[] DRIVE_PID = {0.04, 0, 0, 1 / DRIVE_FREE_SPD};
+    public static final double[] DRIVE_PID = {0.04, 0, 0};
     public static final SimpleMotorFeedforward DRIVE_FEEDFORWARD = new SimpleMotorFeedforward(0.25521, 2.0821, 0.10605);
 
 
