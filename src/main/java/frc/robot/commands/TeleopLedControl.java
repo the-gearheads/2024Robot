51de1cffa914082ea47fcec8a6712cab66c8aafd
person4268 <28717044+person4268@users.noreply.github.com>
2024-03-15 10:20:26 -0400

    gavin i just tested and the old led code worked perfectly fine

--- src/main/java/frc/robot/commands/TeleopLedControl.java
+++ src/main/java/frc/robot/commands/TeleopLedControl.java
@@ -2,8 +2,6 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.feeder.Feeder;
 import frc.robot.subsystems.leds.LedState;
 import frc.robot.subsystems.leds.Leds;
@@ -25,8 +23,10 @@ public class TeleopLedControl extends Command {
   }
   @Override
   public void execute() {
-    feeder.getBeamBreakSwitch().whileTrue(
-      Commands.deadline(new WaitCommand(2), Commands.run(()->leds.setState(LedState.FLASH_LIME), leds))
-    );
+    if (feeder.getBeamBreakSwitch().getAsBoolean()) {
+      leds.setState(LedState.FLASH_LIME);
+    } else {
+      leds.setState(leds.defaultState);
+    }
   }
 }
