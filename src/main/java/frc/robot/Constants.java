296af0a0fa4c1d2577fec0d9a125a0b4546f696a
Ceticx0 <gavinoperry9@gmail.com>
2024-03-29 11:28:50 -0400

    lowered feeding speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -134,7 +134,7 @@ public final class Constants {
     public static final double MAX_SHOOTING_SPEED_VY = 0.3;
     public static final double MAX_SHOOTING_SPEED_ROT = 0.05; // omega rad / s
 
-    public static final double NOTE_FEEDING_SPEED = 3500;
+    public static final double NOTE_FEEDING_SPEED = 3250;
   }
 
   public static class ArmConstants {
