29426cd3ea31780c2606e4408d587c2c2ba166bf
person4268 <28717044+person4268@users.noreply.github.com>
2024-04-26 08:43:36 -0400

    attempt to make runfeedercommand run async with AsynchronousInterrupt (note: doesn't debounce)

--- src/main/java/frc/robot/subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.feeder;
 
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.AsynchronousInterrupt;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,6 +16,8 @@ import frc.robot.subsystems.FlywheelMotor;
 
 import static frc.robot.Constants.FeederConstants.*;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.littletonrobotics.junction.Logger;
 
 public class Feeder extends SubsystemBase {
@@ -22,8 +25,28 @@ public class Feeder extends SubsystemBase {
   public FlywheelMotor handoffMotor = new FlywheelMotor("Handoff", HANDOFF_ID, PID, FEEDER_FF, true, true);
   private DigitalInput beamBreak = new DigitalInput(BEAMBREAK_SWITCH_ID);
   private Trigger beamBreakSwitch = new Trigger(Robot.triggerEventLoop, ()->!beamBreak.get());
+
+  // There's a note in the bot when low, so we only care abt the falling edge (that being said, this should be configued to only act on the falling edge)
+  // After reading the implementation, it seems the callback runs in another thread
+  // Whether the interrupt should stop the feeder when a note hits it.
+  AtomicBoolean shouldStopFeeder = new AtomicBoolean();
+  // A "callback" for the command so it knows that the feeder was stopped by the interrupt
+  AtomicBoolean didStopFeeder = new AtomicBoolean();
+  // Problem: this doesn't debounce. I don't know how to make it (at least in a trivial way).
+  AsynchronousInterrupt bbInterrupt = new AsynchronousInterrupt(beamBreak, (risingEdge, fallingEdge) -> {
+    if(shouldStopFeeder.get() && fallingEdge) {
+      runAtSpeed(0);
+      // Need to run periodic so the pid controllers can run and do their thing, since we want to stop NOW.
+      feederMotor.periodic();
+      handoffMotor.periodic();
+      didStopFeeder.set(true);
+      shouldStopFeeder.set(false);
+    }
+  });
+
   public Feeder() {
     SmartDashboard.putNumber("Feeder/RunSpeed", SPEED);
+    bbInterrupt.setInterruptEdges(false, true);
   }
 
   public void periodic() {
@@ -33,6 +56,8 @@ public class Feeder extends SubsystemBase {
     handoffMotor.log();
 
     Logger.recordOutput("Feeder/beamBreakSwitch", beamBreakSwitch.getAsBoolean());
+    Logger.recordOutput("Feeder/ShouldStopFeeder", shouldStopFeeder.get());
+    Logger.recordOutput("Feeder/DidStopFeeder", didStopFeeder.get());
   }
   
   public void run() {
@@ -54,11 +79,19 @@ public class Feeder extends SubsystemBase {
   }
 
   public Command getRunFeederCommand(double seconds) {
-    return Commands.run(this::run, this).andThen(new WaitCommand(seconds)).andThen(Commands.run(this::stop, this));
+    return this.run(this::run)
+      .andThen(new WaitCommand(seconds))
+      .andThen(Commands.run(this::stop, this))
+      .withName("RunFeederCommand" + seconds + "Seconds");
   }
 
   public Command getRunFeederCommand() {
-    return Commands.run(this::run, this).until(this.getBeamBreakSwitch().debounce(0.08).negate());
+    return this.runOnce(()->{
+      shouldStopFeeder.set(true);
+      didStopFeeder.set(false);
+    }).andThen(
+      this.run(this::run).until(didStopFeeder::get)
+    ).withName("RunFeederCommandAsync");
   }
 
   public Trigger getBeamBreakSwitch() {
