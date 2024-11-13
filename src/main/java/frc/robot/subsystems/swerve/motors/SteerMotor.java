ef982d300c9cdfc954218d51e746001f8a3a8417
person4268 <28717044+person4268@users.noreply.github.com>
2024-11-13 09:21:24 -0500

    update studica to beta3

--- src/main/java/frc/robot/subsystems/swerve/motors/SteerMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/motors/SteerMotor.java
@@ -16,7 +16,6 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.REVLibError;
 
 import edu.wpi.first.math.geometry.Rotation2d;
